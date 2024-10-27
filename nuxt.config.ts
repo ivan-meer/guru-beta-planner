b50405c6b1282b5dd6b1b5f7c2aea0a20b07e90a
ivan-meer <112258381+ivan-meer@users.noreply.github.com>
2024-10-27 19:58:47 +0700

    add DB

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxthub/core'],
-
+  hub: {
+    database: true
+  },
   routeRules: {
     '/': { prerender: true }
   },
-
   compatibilityDate: '2024-10-26'
 })
-
