691e0e6b944511342a81c67da79667b486d329b1
ivan-meer <112258381+ivan-meer@users.noreply.github.com>
2024-10-27 20:30:40 +0700

    no

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxthub/core'],
+  modules: ['@nuxt/content', '@nuxthub/core', '@nuxthq/studio'],
   routeRules: {
     '/': { prerender: true }
   },
